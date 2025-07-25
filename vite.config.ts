1b6b13d1716efe9f9a082f0e0b22e07191c81baa
alyonium <alyonium2.0@gmail.com>
2025-07-25 10:36:20 +0200

    remove unused directories

--- vite.config.ts
+++ vite.config.ts
@@ -16,9 +16,7 @@ export default defineConfig({
     alias: {
       src: path.resolve(__dirname, 'src'),
       api: path.resolve(__dirname, 'src/api'),
-      assets: path.resolve(__dirname, 'src/assets'),
       components: path.resolve(__dirname, 'src/components'),
-      hooks: path.resolve(__dirname, 'src/hooks'),
       modules: path.resolve(__dirname, 'src/modules'),
       router: path.resolve(__dirname, 'src/router'),
       types: path.resolve(__dirname, 'src/types'),
